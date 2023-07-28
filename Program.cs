dffa310f96262344d827240c1b0af8b0414f756e
deepakjayakumar <deepakjayakumar11@gmail.com>
2023-07-28 12:26:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace BuilderDesignPatternDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.AppendLine("<p>");
+            sb.AppendLine(hello);
+            sb.AppendLine("</p>");
+
+            Console.WriteLine(sb);
+        }
+    }
+}
